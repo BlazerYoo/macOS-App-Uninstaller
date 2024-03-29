87c6138e9f9979e7ec02410d1a978c0dbba57669
BlazerYoo <blazeryoo1921@gmail.com>
2024-03-29 05:08:41 -0400

    Update delete_app.sh

--- delete_app.sh
+++ delete_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ask_to_delete() {
-	echo "Delete file(y/n)?"
+	echo "Delete file(y/n/o)?"
 	echo "$1"
 }
 
@@ -9,12 +9,16 @@ need_to_delete() {
 
 	ask_to_delete "$1"
 
-	read -p "Input (y/n):" answer < /dev/tty
+	read -p "Input (y/n/o):" answer < /dev/tty
 
 	if echo "$answer" | grep "^y"; then
 		return 0
-	else 
+	elif echo "$answer" | grep "^o"; then
 		return 1
+	elif echo "$answer" | grep "^n"; then
+		return 2
+	else 
+		return 3
 	fi
 }
 
@@ -22,7 +26,7 @@ is_empty() {
 	if [ -s "$1" ]; then
 		return 1
 	else
-	    return 0
+		return 0
 	fi
 }
 
@@ -40,8 +44,20 @@ if is_empty "$related_files"; then
 fi
 
 cat "$related_files" | while read filename; do
-	if need_to_delete "$filename"; then
+	echo "-------------------------"
+	need_to_delete "$filename"
+	response=$?
+	if [ $response -eq 0 ]; then
+		echo "  Deleting $filename"
 		delete "$filename"
+	elif [ $response -eq 1 ]; then
+		echo "  Opening"
+		parent_dir=$(dirname "$filename")
+		open $parent_dir
+	elif [ $response -eq 2 ]; then
+		echo "  Keeping $filename"
+	else
+		echo "  Invalid input. File skipped"
 	fi
 done
 
